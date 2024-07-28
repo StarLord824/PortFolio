9dc6ed40bc795b542f76ec7a071cc548d4864193
StarLord <shuklajiabhi869@gmail.com>
2024-07-28 18:38:34 +0530

    1st Stage

---
+++ scripts.js
@@ -0,0 +1,23 @@
+document.querySelector('#main-header').addEventListener('click', function() {
+    document.querySelector('#skills').style.top = '79%';
+    document.querySelector('#projects').style.top = '86%';
+    document.querySelector('#coursework').style.top = '93%';
+});
+
+document.querySelector('#p1').addEventListener('click', function() {
+    document.querySelector('#skills').style.top = '12%';
+    document.querySelector('#projects').style.top = '88%';
+    document.querySelector('#coursework').style.top = '94%';
+});
+
+document.querySelector('#p2').addEventListener('click', function() {
+    document.querySelector('#skills').style.top = '10%';
+    document.querySelector('#projects').style.top = '13%';
+    document.querySelector('#coursework').style.top = '95%';
+});
+
+document.querySelector('#p3').addEventListener('click', function() {
+    document.querySelector('#skills').style.top = '8%';
+    document.querySelector('#projects').style.top = '10%';
+    document.querySelector('#coursework').style.top = '13%';
+});
